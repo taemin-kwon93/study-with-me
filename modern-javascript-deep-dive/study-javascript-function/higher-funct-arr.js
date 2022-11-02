bc22836355f4bd5db9013f6dd2708148d9356b7c
taemin-kwon93 <4146kt@gmail.com>
2022-11-02 22:31:10 +0900

    study: callback and higher function

--- modern-javascript-deep-dive/study-javascript-function/higher-funct-arr.js
+++ modern-javascript-deep-dive/study-javascript-function/higher-funct-arr.js
@@ -23,9 +23,14 @@ var res4 = [1, 2, 3].filter(function (item) {
 console.log(res4); // [ 1, 3 ]
 console.log()
 
-var res5 = [0, 1, 0].reduce(function (acc, cur) {
+var res5 = [2, 3, 4].reduce(function (acc, cur) {
+  /**
+   * [0, 0, 1]일때 4 4 4 나옴, [0, 1, 0]일때 4 4 5, [1, 0, 0]일때 4, 5, 5
+   * [1, 1, 0]일때 4 5 6 res5 = 6, [1, 1, 1]일때 4, 5, 6 res5 = 7
+   * [2, 3, 4]일때 4, 6, 9 res5 = 13
+   */
   console.log(acc);
-  // console.log(cur); // 1 2 3
+  console.log(cur); // [x, y, z]안에 담긴 값이 나옴 1 2 3
   console.log();
 
   return acc + cur;
